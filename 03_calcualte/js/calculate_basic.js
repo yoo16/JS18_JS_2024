043c10fbcf40d8ee69e80d3a44dc7a301faa9e77
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-18 10:05:51 +0900

    update

---
+++ 03_calcualte/js/calculate_basic.js
@@ -0,0 +1,31 @@
+/**
+ * 二項演算
+ */
+var price = 500;
+// TODO: price に 200 を足して再代入
+console.log(price);
+
+// TODO: price に 200 を引いて再代入
+price = price - 200;
+console.log(price);
+
+// TODO: price に 2 をかけて再代入
+console.log(price);
+
+// TODO: price を 2 で割って再代入
+console.log(price);
+
+/**
+ * 単項演算
+ */
+var quantity = 1;
+// TODO: 個数を1増やす
+console.log(quantity);
+
+// TODO: 個数を1減らす
+console.log(quantity);
+
+/**
+ * 複合演算
+ */
+var price = 500;
