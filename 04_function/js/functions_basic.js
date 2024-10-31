f2d72be54bd3f4f6ed5986b9749d18705602def2
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-31 10:07:27 +0900

    update

--- 04_function/js/functions_basic.js
+++ 04_function/js/functions_basic.js
@@ -11,6 +11,19 @@ var itemName = "コーヒー"
 number = parseInt(itemName)
 console.log(number)
 
+var result
+// isNaN() :数字ではないかをチェックする関数
+result = isNaN(numberString)
+console.log(result)
+
+// itemNameをチェック
+result = isNaN(itemName)
+console.log(result)
+
+// numberをチェック
+result = isNaN(10)
+console.log(result)
+
 /**
  * ユーザ定義関数
  */
