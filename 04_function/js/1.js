76763f781aa5d28473e615b3084a68645330fc45
yoo16 <yohei.yoshikawa@gmail.com>
2024-10-31 10:09:38 +0900

    update

--- 04_function/js/1.js
+++
@@ -1,9 +0,0 @@
-var numberString = "0005"
-console.log(numberString)
-
-var number = parseInt(numberString)
-console.log(number)
-
-var itemName = "コーヒー"
-number = parseInt(itemName)
-console.log(number)
