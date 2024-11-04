2307029adb9a9a4630c3a1e1460599570d3455bc
yoo16 <yohei.yoshikawa@gmail.com>
2024-11-04 19:57:38 +0900

    update

---
+++ 08_array/js/array_app.js
@@ -0,0 +1,58 @@
+// TODO: 配列の定義（'コーヒー', '紅茶', 'ほうじ茶'）
+var items = [];
+
+console.log(items);
+
+/**
+ * 配列を文字列で表示
+ */
+function showArrayString() {
+    // 配列をカンマ区切りの文字列に変換
+    var string = items.join(',');
+    document.getElementById('array-string').innerHTML = string;
+}
+
+/**
+ * 配列の要素表示
+ */
+function showItem() {
+    // ブラウザで入力したインデックスを取得
+    const index = document.getElementById('input-index').value;
+    console.log(index)
+
+    // インデックス範囲内かチェック
+    if (index >= 0 && index < items.length) {
+        // TODO: items に index を指定してデータ表示
+        document.getElementById('input-value').value;
+
+        // エラーメッセージ空白
+        document.getElementById('error').innerHTML = "";
+    } else {
+        // 配列の個数から、最大インデックスを設定
+        var maxIndex = items.length - 1;
+        var error = `0 から${maxIndex}の範囲で指定してください。`;
+        document.getElementById('error').innerHTML = error;
+    }
+}
+
+/**
+ * 値の更新
+ */
+function updateItem() {
+    // Indexを取得
+    const index = document.getElementById('input-index').value;
+    // Valueを取得
+    const value = document.getElementById('input-value').value;
+
+    console.log(index, value)
+
+    // TODO: インデックスを指定して値更新
+
+    // 配列の値表示
+    showItem();
+
+    // 配列の文字列表示
+    showArrayString();
+}
+
+showArrayString();
---
+++ 09_array_assoc/js/array_assoc.js
@@ -0,0 +1,48 @@
+const current = document.getElementById('current-station');
+const furigana = document.getElementById('current-furigana');
+const next = document.getElementById('next-station');
+const prev = document.getElementById('prev-station');
+
+// 連想配列（オブジェクト）の定義
+// name = 東京, furigana = とうきょう
+var station
+// DevTools で確認
+console.log(station)
+
+// ブラウザの強制再読み込み
+// Windows: Ctrl + Shift + R
+// Mac: Cmd + Shift + R
+
+// TODO:駅名表示
+current.innerHTML
+furigana.innerHTML
+
+// TODO: 駅名、ふりがなのデータ更新
+// name = 有楽町, furigana = ゆうらくちょう
+
+// TODO: 駅名表示
+current.innerHTML
+furigana.innerHTML
+
+// 連想配列の配列
+// name: 東京, furigana: とうきょう
+// name: 有楽町, furigana: ゆうらくちょう
+// name: 新橋, furigana: しんばし
+const stations = []
+
+console.log(stations)
+
+// 現在の駅名表示
+// index = 1
+var currentStation
+console.log(currentStation)
+
+// TODO: 駅名表示
+current.innerHTML
+furigana.innerHTML
+
+// TODO: 前の駅名表示
+prev.innerHTML
+
+// TODO: 次の駅名表示
+next.innerHTML
