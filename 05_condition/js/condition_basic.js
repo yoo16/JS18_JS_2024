d7feb3f2d305f3bc253ed7866fb208d7a78b0807
yoo16 <yohei.yoshikawa@gmail.com>
2024-11-07 09:52:37 +0900

    update

--- 05_condition/js/condition_basic.js
+++ 05_condition/js/condition_basic.js
@@ -6,7 +6,13 @@ result = (price == 500)
 console.log(result)
 
 // price が 0 より小さいか？
+result = (price < 0)
+console.log(result)
 
 // price が 500 より大きいか？
+result = (price > 500)
+console.log(result)
 
 // price が 500 以上か？
+result = (price >= 500)
+console.log(result)
