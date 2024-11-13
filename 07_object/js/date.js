350c952f02a8ac44027c98202036eb24807e5990
yoo16 <yohei.yoshikawa@gmail.com>
2024-11-14 07:54:04 +0900

    update

--- 07_object/js/date.js
+++ 07_object/js/date.js
@@ -1,9 +1,9 @@
 // 現在の年月日と時刻を表示する関数
 function updateTime() {
-    const now = new Date();
+    // TODO: 現在の日時の作成
+    var now
 
-    // 年月日と時刻を取得
-    // TODO：日時の値を取得
+    // TODO: 年月日と時刻を取得
     var year
     var month
     var day
@@ -14,8 +14,10 @@ function updateTime() {
     // フォーマット YYYY/MM/DD HH:MM:SS
     const formattedDateTime = `${year}/${month}/${day} ${hours}:${minutes}:${seconds}`;
 
+    // id=datetime の Element取得
     const datetimeElement = document.getElementById("datetime");
 
+    // 日時を設定
     datetimeElement.textContent = formattedDateTime;
 }
 
