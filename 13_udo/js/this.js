133a7b4814b39ae1d7951fc33706f4f4a67094ad
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-12 09:57:26 +0900

    update

--- 13_udo/js/this.js
+++ 13_udo/js/this.js
@@ -1,13 +1,21 @@
+// ブラウザ強制再読み込み
+// Mac: Cmd + Shift + R
+// Win: Ctrl + Shift + R
 // TODO: thisオブジェクト（グローバル）
+console.log(this)
 
 // TODO: 現在のURL
-document.getElementById('window-href').innerText;
+document.getElementById('window-href').innerText
+    = this.location.href;
+    // = location.href;
 
 // TODO: ユーザエージェント
-document.getElementById('window-userAgent').innerText;
+document.getElementById('window-userAgent').innerText
+    = this.navigator.userAgent
 
 // TODO: ウィンドウサイズ
-document.getElementById('window-size').innerText;
+document.getElementById('window-size').innerText
+    = this.screen.width + ', ' + this.screen.height;
 
 function clickEvent(element, event) {
     // Element取得
