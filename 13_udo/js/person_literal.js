001278863a511817cc2620f255dbed0c5d327e07
yoo16 <yohei.yoshikawa@gmail.com>
2024-12-12 10:23:53 +0900

    udpate

--- 13_udo/js/person_literal.js
+++ 13_udo/js/person_literal.js
@@ -9,10 +9,13 @@ const person = {
 
 // TODO: オブジェクトリテラルで処理
 // name
-document.getElementById('person-name').innerHTML = person.name;
+document.getElementById('person-name').innerHTML
+    = person.name;
 
 // age
-document.getElementById('person-age').innerHTML = person.age;
+document.getElementById('person-age').innerHTML
+    = person.age;
 
 // greet()
-document.getElementById('person-greet').innerHTML;
+document.getElementById('person-greet').innerHTML
+    = person.greet();
